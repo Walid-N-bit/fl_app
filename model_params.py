e171710cf4d416a4fd4281f61b58e565349cbe43
Walid-N-bit <wnouicer24@student.oulu.fi>
2026-01-22 09:30:00 +0100

    re-track necessary repo files

---
+++ model_params.py
@@ -0,0 +1,19 @@
+CLASSES = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+DATASET_ID = "uoft-cs/cifar10"
+IMG_C = 3  # in_channels
+IMG_H = 32
+IMG_W = 32
+
+OUTPUT_CHANNELS = 16  # was 3
+KERNEL_SIZE = 5
