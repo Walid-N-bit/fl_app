83bb98be24889f4c2037b72ec6a51d9758c21572
Walid-N-bit <wnouicer24@student.oulu.fi>
2026-02-26 17:08:54 +0200

    _

--- server_app.py
+++ server_app.py
@@ -56,7 +56,7 @@ def main(grid: Grid, context: Context) -> None:
         "freeze": freeze,
         "batch-size": batch_size,
         "use-sampler": use_sampler,
-        "num-workers": (0 if DEVICE == "cpu" else num_workers),
+        "num-workers": 0,
         "features-lr": features_lr,
         "classifier-lr": classifier_lr,
         "weight-decay": weight_decay,
